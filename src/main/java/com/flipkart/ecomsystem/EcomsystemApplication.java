3d773933dd38e8105359725ae206ee71fbf50788
Geetika <geethika.singh20>
2025-09-17 11:23:11 +0530

    spring boot project commit

---
+++ src/main/java/com/flipkart/ecomsystem/EcomsystemApplication.java
@@ -0,0 +1,13 @@
+package com.flipkart.ecomsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flipkart/ecomsystem/EcomsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flipkart.ecomsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
